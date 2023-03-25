84d3b9defb17536c4e09d7e4a51056d79e294f9c
Kaneknah <jamesnbaxley@protonmail.com>
2023-03-24 20:39:35 -0600

    updated webpack with plugins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,23 +8,44 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
-    ],
+		mode: "development",
+		entry: {
+			main: "./src/js/index.js",
+			install: "./src/js/install.js",
+		},
+		output: {
+			filename: "[name].bundle.js",
+			path: path.resolve(__dirname, "dist"),
+		},
+		plugins: [
+			// Webpack plugin that generates our html file.
+			new HtmlWebpackPlugin({
+				template: "./index.html",
+				title: "Contact Cards",
+			}),
+		],
 
-    module: {
-      rules: [
-        
-      ],
-    },
-  };
+		module: {
+			rules: [
+				{
+					test: /\.css$/i,
+					use: ["style-loader", "css-loader"],
+				},
+				{
+					test: /\.m?js$/,
+					exclude: /node_modules/,
+					use: {
+						loader: "babel-loader",
+						options: {
+							presets: ["@babel/preset-env"],
+							plugins: [
+								"@babel/plugin-proposal-object-rest-spread",
+								"@babel/transform-runtime",
+							],
+						},
+					},
+				},
+			],
+		},
+	};
 };
