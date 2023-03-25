41b801845fcb82873cbed28de37a4a0e5086daa8
Kaneknah <jamesnbaxley@protonmail.com>
2023-03-24 20:24:40 -0600

    updated database code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,17 +15,17 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.error('putDb not implemented');
- // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('contact', 1);
+ // Create a connection to the database.
+  const editorDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('contact', 'readonly');
+  const tx = editorDb.transaction('jate', 'readonly');
 
   // Open up the desired object store.
-  const store = tx.objectStore('contact');
+  const store = tx.objectStore('jate');
 
-  // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
+  // Use the .put() method to get all data in the database.
+  const request = store.put({ id: 1, value: content });
 
   // Get confirmation of the request.
   const result = await request;
@@ -35,21 +35,21 @@ export const putDb = async (content) => {
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.error('getDb not implemented');
-// Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('contact', 1);
+// Create a connection to the database.
+  const editorDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('contact', 'readwrite');
+  const tx = editorDb.transaction('jate', 'readwrite');
 
   // Open up the desired object store.
-  const store = tx.objectStore('contact');
+  const store = tx.objectStore('jate');
 
-  // Use the .delete() method to get all data in the database.
-  const request = store.delete(id);
+  // Use the .get() method to get all data in the database.
+  const request = store.get(1);
 
   // Get confirmation of the request.
   const result = await request;
   console.log('result.value', result);
-  return result?.value;
+  return result;
 };
 initdb();
